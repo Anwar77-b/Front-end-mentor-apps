85a0ef0775dcd0e7390ac816ccc3197e3862e6d8
Anwar77-b <abdennourbouzid5@gmail.com>
2024-07-03 10:37:36 +0100

    first commit

---
+++ space-tourism-app/src/Components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (<>
+    <header>
+        <nav>
+            <div className="logo"></div>
+            <ul className="pages">
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+    </header>
+  </>);
+}
